49a9859a0c512db881e1b68b7e29db5335944717
Salman Alzaid <s.alzaid@acg.edu>
2024-07-09 15:46:14 +0300

    V 1.0 (created a functional login/logout)

---
+++ JerseyHeritage/store/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+# Create your views here.
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products':products})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def login_user(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You successfully Logged in! "))
+            return redirect('home')
+        
+        else:
+            messages.success(request, ("The Username or Password is incorrect Please try again "))
+            return redirect('login')
+    else:
+        
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("you successfully loged out !"))
+    return redirect('home')
