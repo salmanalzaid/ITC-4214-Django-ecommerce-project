972c18bbb2c2724dbc47c90467d7f0bf43dba6ac
Salman Alzaid <s.alzaid@acg.edu>
2024-07-12 00:25:26 +0300

    V 1.2 everything functional except search for items  button

--- JerseyHeritage/cart/views.py
+++ JerseyHeritage/cart/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
@@ -10,10 +11,12 @@ def cart_summary(request):
     #get the cart
     cart = Cart(request)
     cart_products = cart.see_products
+    quantities = cart.get_quants
+    totals = cart.cart_total()
     
     
     
-    return render(request, "cart_summary.html", {"cart_products":cart_products})
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
 def cart_add(request):
     #get the cart
@@ -22,24 +25,48 @@ def cart_add(request):
     if request.POST.get('action') == 'post' :
         #get the product
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         #look for product
         product = get_object_or_404(Product, id=product_id)
         
         #savr the the add to carts to session
-        cart.add(product=product)
+        cart.add(product=product, quantity= product_qty)
         
         cart_quantity = cart.__len__()
         
         #response = JsonResponse({'Product Name:': product.name})
 
         response = JsonResponse({' qty': cart_quantity})
+        messages.success(request, ("Product added sucessfully !!"))
+
         return response
         
     
     
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        cart.delete(product = product_id)
+            
+        response = JsonResponse({'product':product_id})
+        messages.success(request, ("Your item has been deleted"))
+
+        return response
 
 def cart_update(request):
-    pass
+	cart = Cart(request)
+	if request.POST.get('action') == 'post':
+		# Get stuff
+		product_id = int(request.POST.get('product_id'))
+		product_qty = int(request.POST.get('product_qty'))
+
+		cart.update(product=product_id, quantity=product_qty)
+
+		response = JsonResponse({'qty':product_qty})
+		#return redirect('cart_summary')
+		messages.success(request, ("Your Cart Has Been Updated..."))
+		return response
+        
+    
--- JerseyHeritage/store/views.py
+++ JerseyHeritage/store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 from django import forms
 # Create your views here.
 
@@ -27,6 +27,7 @@ def category(request, look):
 
         return redirect('home')
         
+#logging in account 
 
 def login_user(request):
     
@@ -46,13 +47,13 @@ def login_user(request):
         
         return render(request, 'login.html', {})
 
-
+# logging out 
 def logout_user(request):
     logout(request)
     messages.success(request, ("you successfully loged out !"))
     return redirect('home')
 
-
+#registering a new user
 def register_user(request):
     
     form = SignUpForm()
@@ -78,3 +79,65 @@ def register_user(request):
 def product(request, pk):
     product = Product.objects.get(id=pk)
     return render(request, 'product.html', {'product':product})
+
+
+# update the profile 
+
+def update_user(request):
+	if request.user.is_authenticated:
+		current_user = User.objects.get(id=request.user.id)
+		user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+		if user_form.is_valid():
+			user_form.save()
+
+			login(request, current_user)
+			messages.success(request, "User Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_user.html", {'user_form':user_form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
+
+#update password 
+def update_password(request):
+	if request.user.is_authenticated:
+		current_user = request.user
+		# Did they fill out the form
+		if request.method  == 'POST':
+			form = ChangePasswordForm(current_user, request.POST)
+			# Is the form valid
+			if form.is_valid():
+				form.save()
+				messages.success(request, "Your Password Has Been Updated Successfully ")
+				login(request, current_user)
+				return redirect('update_user')
+			else:
+				for error in list(form.errors.values()):
+					messages.error(request, error)
+					return redirect('update_password')
+		else:
+			form = ChangePasswordForm(current_user)
+			return render(request, "update_password.html", {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+
+#search for products
+
+def search(request):
+    
+	if request.method == "POST":
+     
+		searched = request.POST['searched']
+		#searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+  
+		if not searched:
+			messages.success(request, "That Product Does Not Exist...Please try Again.")
+			return render(request, "search.html", {})
+
+		else:
+			return render(request, "search.html", {'searched':searched})
+
+	else:
+		return render(request, "search.html", {})
